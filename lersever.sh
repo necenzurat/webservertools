a90055423f03b39de649b0a311f7ef34270144b1
necenzurat <necenzura@gmail.com>
2013-07-15 18:07:36 +0000

    beautify readme.md

--- lersever.sh
+++ lersever.sh
@@ -30,17 +30,17 @@ apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 
 #update and remove apache
 apt-get update && apt-get upgrade
-
+~
 #install nginx and others
 
-apt-get install -y nginx percona-server-server-5.5 percona-server-client-5.5
+apt-get install nginx percona-server-server-5.5 percona-server-client-5.5
 
 # optional 
 # apt-get install memcached
 
 
 # php shits
-apt-get install -y php5 php5-cli php5-common php5-curl php5-dev php5-gd php5-curl libcurl4-openssl-dev php5-mcrypt php5-fpm build-essential php5-memcached
+apt-get install php5 php5-cli php5-common php5-curl php5-dev php5-gd php5-curl libcurl4-openssl-dev php5-mcrypt php5-fpm build-essential php5-memcached
 
 apt-get remove apache2*
 
--- readme.md
+++ readme.md
@@ -1,14 +1,33 @@
+LeInstall
+================
 Debian, nginx, Percona, PHP-FPM install script
-===
+
+le = the
+install = quick deploy nginx, php-fpm, percona
 
 
-todo:
+
+Installation
+------------
+
+1. wget-it
+2. run it
+2.5 wait some minutes/seconds
+3. enjoy your server
+
+
+
+
+Installation
+------------
+
 * validate stuff
 * automatic install if specified 
 * ask for forgivnes... permission
 * other stuff
 
 
- 
+Bugs
+------------
 
-
+* it seems that the percona instalation does not work
