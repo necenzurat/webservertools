a0d9d3aa6c199f1b8e1068621494c6174ddbb5eb
SaltwaterC <saltwaterc@gmail.com>
2013-07-17 10:14:51 +0300

    Kill apache2 for good before hitting the network with apt. This also removes the auto-dependencies and config.

--- lersever.sh
+++ lersever.sh
@@ -8,6 +8,9 @@ function check_root() {
 }
 check_root
 
+# remove apache2
+apt-get autoremove --purge apache2*
+
 #backports
 cat > /etc/apt/sources.list.d/backports.sources.list <<END
 deb http://ftp.ro.debian.org/debian/ wheezy-backports main contrib non-free
@@ -36,16 +39,12 @@ apt-get update && apt-get upgrade
 
 apt-get install nginx percona-server-server-5.5 percona-server-client-5.5
 
-# optional 
+# optional
 # apt-get install memcached
 
 
 # php shits
 apt-get install php5 php5-cli php5-common php5-curl php5-dev php5-gd php5-curl libcurl4-openssl-dev php5-mcrypt php5-fpm build-essential php5-memcached
-
-apt-get remove apache2*
-
-
 
 service mysql start
 service nginx start
