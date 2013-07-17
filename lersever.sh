4db95fc889688a77af367794714eae6c904635a6
SaltwaterC <saltwaterc@gmail.com>
2013-07-17 10:21:27 +0300

    Cleaned up the commentz and shitz.

--- lersever.sh
+++ lersever.sh
@@ -11,35 +11,33 @@ check_root
 # remove apache2
 apt-get autoremove --purge apache2*
 
-#backports
+# backports
 cat > /etc/apt/sources.list.d/backports.sources.list <<END
 deb http://ftp.ro.debian.org/debian/ wheezy-backports main contrib non-free
 END
 
-#nignx
+# nginx
 cat > /etc/apt/sources.list.d/nginx.sources.list <<END
 deb http://nginx.org/packages/debian/ wheezy nginx
 deb-src http://nginx.org/packages/debian/ wheezy nginx
 END
 wget -q http://nginx.org/keys/nginx_signing.key -O - | apt-key add -
 
-#percona
+# percona
 cat > /etc/apt/sources.list.d/percona.sources.list <<END
 deb http://repo.percona.com/apt wheezy main
 deb-src http://repo.percona.com/apt wheezy main
 END
 apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 
-#update and remove apache
+# upgrade the system
 apt-get update && apt-get dist-upgrade
-~
-#install nginx and others
 
+# install nginx and others
 apt-get install nginx percona-server-server-5.5 percona-server-client-5.5
 
 # optional
 # apt-get install memcached
-
 
 # php shits
 apt-get install php5 php5-cli php5-common php5-curl php5-dev php5-gd php5-curl libcurl4-openssl-dev php5-mcrypt php5-fpm build-essential php5-memcached
